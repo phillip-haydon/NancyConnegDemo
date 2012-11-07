0d39f4fb374ec2125546c63e1475e02f916f3465
Phillip Haydon <phillip.haydon@gmail.com>
2012-11-07 23:36:50 +0800

    adding demo project for content negotiation

---
+++ NancyConnegDemo/Modules/HomeModule.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using Nancy;
+
+namespace NancyConnegDemo.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public static IList<Product> Products = new List<Product>
+        {
+            new Product {Id = 1, Name = "Surface", Price = 499},
+            new Product {Id = 2, Name = "iPad", Price = 899},
+            new Product {Id = 3, Name = "Nexus 10", Price = 599},
+            new Product {Id = 4, Name = "Think Pad", Price = 499},
+            new Product {Id = 5, Name = "Yoga", Price = 699},
+        };
+
+        public dynamic Model = new ExpandoObject();
+
+        public HomeModule()
+        {
+            Model.Deleted = false;
+
+            Get["/"] = _ =>
+            {
+                Model.Products = Products;
+
+                return View["index", Model];
+            };
+
+            Get[@"/delete/{id}"] = _ =>
+            {
+                var id      = (int) _.id;
+                var item    = Products.Single(x => x.Id == id);
+                
+                Products.Remove(item);
+
+                Model.Products = Products;
+                Model.Deleted = true;
+
+                return Negotiate
+                    .WithModel((object) Model)
+                    .WithMediaRangeModel("application/json", new
+                    {
+                        Model.Deleted
+                    })
+                    .WithView("index");
+            };
+        }
+    }
+}
---
+++ NancyConnegDemo/NancyDemoBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.Conventions;
+using Nancy.Diagnostics;
+using Nancy.TinyIoc;
+
+namespace NancyConnegDemo
+{
+    public class NancyDemoBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = @"test" }; }
+        }
+
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("Scripts", @"Scripts", ".js")
+            );
+        }
+    }
+}
